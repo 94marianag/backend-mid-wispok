70be192dc3f520c6955280f8168197790fe01f22
Mariana <94marianag@gmail.com>
2025-09-24 07:26:12 -0600

    update DTO

--- src/dtos/createPayment.dto.ts
+++ src/dtos/createPayment.dto.ts
@@ -1,4 +1,10 @@
-import { IsPositive, IsString, IsNumber, IsEnum } from "class-validator";
+import {
+  IsPositive,
+  IsString,
+  IsNumber,
+  IsEnum,
+  IsNotEmpty,
+} from "class-validator";
 import { PaymentMethod } from "../mongo/models/Payment";
 
 export class CreatePaymentDto {
@@ -7,6 +13,7 @@ export class CreatePaymentDto {
   amount!: number;
 
   @IsString({ message: "reference must be a string" })
+  @IsNotEmpty({ message: "reference should not be empty" })
   reference!: string;
 
   @IsEnum(PaymentMethod, { message: "method invalid cash | card | transfer" })
